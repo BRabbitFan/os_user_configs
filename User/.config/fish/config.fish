c520de4e5eda96613142375980ec959a4d689a10
fanjunhong <fanjunhong@mova-tech.com>
2026-04-28 12:09:44 +0800

    更新 alacritty 主题配置，删除旧主题并添加新主题路径；优化 fish 配置，调整主题设置和环境变量管理

--- User/.config/fish/config.fish
+++ User/.config/fish/config.fish
@@ -1,23 +1,35 @@
-if status is-interactive
-  # Commands to run in interactive sessions can go here
-end
-
+# unix locale settings
 set -x LANG zh_CN.UTF-8
 set -x LC_ALL zh_CN.UTF-8
 
-set -g theme_nerd_fonts yes
-set -g theme_color_scheme nord
-set -g theme_display_date no
-set -g theme_display_user yes
-set -g theme_display_hostname no
-set -g theme_display_virtualenv yes  #yes
-set -g theme_display_node no  #always
-set -g theme_title_use_abbreviated_path yes
-set -g theme_git_default_branches no
-set -g theme_project_dir_length 1
-set -g fish_prompt_pwd_dir_length 1
+# python virtualenv
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1  # disable default prompt (use fish theme instead)
+source ~/pyenv/bin/activate.fish
 
-set -x VIRTUAL_ENV_DISABLE_PROMPT 1
-source ~/.pyvenv/bin/activate.fish
+# nodejs version management (fnm)
+set FNM_PATH "$HOME/.local/share/fnm"
+if test -d "$FNM_PATH"
+  set -gx PATH "$FNM_PATH" $PATH
+  eval (fnm env --shell=fish)
+end
 
+# rust package management (cargo)
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# wsltty
 set -x DONT_PROMPT_WSL_INSTALL 1
+
+if status is-interactive
+  # fish theme settings
+  set -g theme_nerd_fonts yes
+  set -g theme_color_scheme nord
+  set -g theme_display_date no
+  set -g theme_display_user yes
+  set -g theme_display_hostname no
+  set -g theme_display_virtualenv yes
+  set -g theme_display_node no  #always
+  set -g theme_title_use_abbreviated_path yes
+  set -g theme_git_default_branches no
+  set -g theme_project_dir_length 1
+  set -g fish_prompt_pwd_dir_length 1
+end
