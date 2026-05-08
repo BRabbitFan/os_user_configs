92d7a03a3f09b248ff3eea4e55413be994d422c9
fanjunhong <fanjunhong@mova-tech.com>
2026-05-08 17:40:33 +0800

    add starship config

--- User/.config/fish/config.fish
+++ User/.config/fish/config.fish
@@ -21,16 +21,28 @@ export PATH="$HOME/.cargo/bin:$PATH"
 set -x DONT_PROMPT_WSL_INSTALL 1
 
 if status is-interactive
-  # fish theme settings
-  set -g theme_nerd_fonts yes
-  set -g theme_color_scheme nord
-  set -g theme_display_date no
-  set -g theme_display_user yes
-  set -g theme_display_hostname no
-  set -g theme_display_virtualenv yes
-  set -g theme_display_node no  #always
-  set -g theme_title_use_abbreviated_path yes
-  set -g theme_git_default_branches no
-  set -g theme_project_dir_length 1
-  set -g fish_prompt_pwd_dir_length 1
+  # starship
+  # curl -sS https://starship.rs/install.sh | sh
+  # pacman -S starship
+  # apt install starship
+  # brew install starship
+  # winget install --id Starship.Starship
+  # eval "$(starship init bash)"                    # ~/.bashrc
+  # eval "$(starship init zsh)"                     # ~/.zshrc
+  # starship init fish | source                     # ~/.config/fish/config.fish
+  # Invoke-Expression (&starship init powershell)   # $PROFILE
+  starship init fish | source
+
+  # omf theme bobthefish
+  # set -g theme_nerd_fonts yes
+  # set -g theme_color_scheme nord
+  # set -g theme_display_date no
+  # set -g theme_display_user yes
+  # set -g theme_display_hostname no
+  # set -g theme_display_virtualenv yes
+  # set -g theme_display_node no  #always
+  # set -g theme_title_use_abbreviated_path yes
+  # set -g theme_git_default_branches no
+  # set -g theme_project_dir_length 1
+  # set -g fish_prompt_pwd_dir_length 1
 end
