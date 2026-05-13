fff6870a53e4cb8d863eadfe5a8e6d33358f875d
fanjunhong <fanjunhong@mova-tech.com>
2026-05-13 10:45:30 +0800

    update

--- User/.config/fish/config.fish
+++ User/.config/fish/config.fish
@@ -1,10 +1,10 @@
 # unix locale settings
-# set -x LANG zh_CN.UTF-8
-# set -x LC_ALL zh_CN.UTF-8
+#set -x LANG zh_CN.UTF-8
+#set -x LC_ALL zh_CN.UTF-8
 
 # python virtualenv
-set -x VIRTUAL_ENV_DISABLE_PROMPT 1  # disable default prompt (use fish theme instead)
-source ~/pyenv/bin/activate.fish
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1 # disable default prompt (use fish theme instead)
+source ~/.pyvenv/bin/activate.fish
 
 # nodejs version management (fnm)
 set FNM_PATH "$HOME/.local/share/fnm"
@@ -18,7 +18,7 @@ export PATH="$HOME/.cargo/bin:$PATH"
 # source /opt/homebrew/opt/rustup/share/fish/vendor_completions.d/rustup.fish  # macos rustup completions
 
 # wsltty
-set -x DONT_PROMPT_WSL_INSTALL 1
+#set -x DONT_PROMPT_WSL_INSTALL 1
 
 if status is-interactive
   # starship
@@ -31,18 +31,18 @@ if status is-interactive
   # eval "$(starship init zsh)"                     # ~/.zshrc
   # starship init fish | source                     # ~/.config/fish/config.fish
   # Invoke-Expression (&starship init powershell)   # $PROFILE
-  starship init fish | source
+  #starship init fish | source
 
   # omf theme bobthefish
-  # set -g theme_nerd_fonts yes
-  # set -g theme_color_scheme nord
-  # set -g theme_display_date no
-  # set -g theme_display_user yes
-  # set -g theme_display_hostname no
-  # set -g theme_display_virtualenv yes
-  # set -g theme_display_node no  #always
-  # set -g theme_title_use_abbreviated_path yes
-  # set -g theme_git_default_branches no
-  # set -g theme_project_dir_length 1
-  # set -g fish_prompt_pwd_dir_length 1
+  set -g theme_nerd_fonts yes
+  set -g theme_color_scheme nord
+  set -g theme_display_date no
+  set -g theme_display_user yes
+  set -g theme_display_hostname no
+  set -g theme_display_virtualenv yes
+  set -g theme_display_node no #always
+  set -g theme_title_use_abbreviated_path yes
+  set -g theme_git_default_branches no
+  set -g theme_project_dir_length 1
+  set -g fish_prompt_pwd_dir_length 1
 end
